22849c9b313cc100c1ff96d2fa561bef7cfba6c7
qli94 <103575941+qli94@users.noreply.github.com>
2022-04-21 22:30:33 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -12,4 +12,4 @@ let borderUISize = game.config.height / 15;
 let borderPadding = borderUISize / 3;
 
 // reserve keyboard variables
-let keyF, keyR, keyLEFT, keyRIGHT;
+let keyA, keyS, keyF, keyR, keyLEFT, keyRIGHT;
