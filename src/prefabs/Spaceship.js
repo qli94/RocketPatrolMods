7fabe0ecf82596897b725f1170817617dd5b6246
qli94 <103575941+qli94@users.noreply.github.com>
2022-04-26 23:26:43 +0800

    Update Spaceship.js

--- src/prefabs/Spaceship.js
+++ src/prefabs/Spaceship.js
@@ -4,14 +4,39 @@ class Spaceship extends Phaser.GameObjects.Sprite {
         super(scene, x, y, texture, frame);
         scene.add.existing(this);   // add to existing scene
         this.points = pointValue;   // store pointValue
-        this.moveSpeed = 10;
+        this.moveSpeed = game.settings.spaceshipSpeed;         // pixels per frame
     }
+
+
 
     update() {
         // move spaceship left
         this.x -= this.moveSpeed;
         // wrap around from left edge to right edge
         if(this.x <= 0 - this.width) {
+            this.reset();
+        }
+    }
+
+    // position reset
+    reset() {
+        this.x = game.config.width;
+    }
+}
+
+class newSpaceship extends Phaser.GameObjects.Sprite {
+    constructor(scene, x, y, texture, frame, pointValue) {
+        super(scene, x, y, texture, frame);
+        scene.add.existing(this);   
+        this.points = pointValue;  
+        this.moveSpeed = game.settings.spaceshipSpeed * 4;       
+    }
+
+    update() {
+ 
+        this.x -= this.moveSpeed;
+       
+        if(this.x <= 120 - this.width) {
             this.reset();
         }
     }
