6ba00ea58a214f57682dc63539ed89476c3e39aa
qli94 <103575941+qli94@users.noreply.github.com>
2022-04-27 00:58:28 +0800

    Update Rocket.js

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -1,24 +1,20 @@
-// Rocket prefab
 class Rocket extends Phaser.GameObjects.Sprite {
-    constructor(scene, x, y, texture, frame, keyL, keyR) {
+    constructor(scene, x, y, texture, frame) {
         super(scene, x, y, texture, frame);
 
         scene.add.existing(this);   // add to existing, displayList, updateList
         this.isFiring = false;      // track rocket's firing status
-        this.horizMoveSpeed = 10;
-        this.moveSpeed = 15;         // pixels per frame
+        this.moveSpeed = 10;         // pixels per frame
         this.sfxRocket = scene.sound.add('sfx_rocket')  // add rocket sfx
-        this.keyLft = keyL;
-        this.keyRht = keyR;
     }
 
     update() {
         // left/right movement
         if(!this.isFiring) {
-            if(this.keyLft.isDown && this.x >= borderUISize + this.width) {
-                this.x -= this.horizMoveSpeed;
-            } else if (this.keyRht.isDown && this.x <= game.config.width - borderUISize - this.width) {
-                this.x += this.horizMoveSpeed;
+            if(keyLEFT.isDown && this.x >= borderUISize + this.width) {
+                this.x -= this.moveSpeed;
+            } else if (keyRIGHT.isDown && this.x <= game.config.width - borderUISize - this.width) {
+                this.x += this.moveSpeed;
             }
         }
         // fire button
@@ -42,4 +38,43 @@ class Rocket extends Phaser.GameObjects.Sprite {
         this.y = game.config.height - borderUISize - borderPadding;
     }
 }
+class Rocket1 extends Phaser.GameObjects.Sprite {
+    constructor(scene, x, y, texture, frame) {
+        super(scene, x, y, texture, frame);
 
+        scene.add.existing(this);   // add to existing, displayList, updateList
+        this.isFiring = false;      // track rocket's firing status
+        this.moveSpeed = 10;         // pixels per frame
+        this.sfxRocket = scene.sound.add('sfx_rocket')  // add rocket sfx
+    }
+
+    update() {
+        // left/right movement
+        if(!this.isFiring) {
+            if(keyA.isDown && this.x >= borderUISize + this.width) {
+                this.x -= this.moveSpeed;
+            } else if (keyD.isDown && this.x <= game.config.width - borderUISize - this.width) {
+                this.x += this.moveSpeed;
+            }
+        }
+        // fire button
+        if(Phaser.Input.Keyboard.JustDown(keySPACE) && !this.isFiring) {
+            this.isFiring = true;
+            this.sfxRocket.play();
+        }
+        // if fired, move up
+        if(this.isFiring && this.y >= borderUISize * 3 + borderPadding) {
+            this.y -= this.moveSpeed;
+        }
+        // reset on miss
+        if(this.y <= borderUISize * 3 + borderPadding) {
+            this.reset();
+        }
+    }
+
+    // reset rocket to "ground"
+    reset() {
+        this.isFiring = false;
+        this.y = game.config.height - borderUISize - borderPadding;
+    }
+}
