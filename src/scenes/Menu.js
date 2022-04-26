000532347b6da88575daf91d3b84af0e73ef9e5d
qli94 <103575941+qli94@users.noreply.github.com>
2022-04-27 02:29:01 +0800

    Update Menu.js

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -30,7 +30,7 @@ class Menu extends Phaser.Scene {
         this.add.text(game.config.width/2, game.config.height/2, 'Use ←→ arrows to move & (F) to fire', menuConfig).setOrigin(0.5);
         this.add.text(game.config.width/2, game.config.height/2 - borderUISize - borderPadding, 'Use A&D arrows to move & SPACE to fire', menuConfig).setOrigin(0.5);
         
-        menuConfig.backgroundColor = '#00FF00';
+        menuConfig.backgroundColor = '#33cc99';
         menuConfig.color = '#000';
         this.add.text(game.config.width/2, game.config.height/2 + borderUISize + borderPadding, 'Press ← for Novice or → for Expert', menuConfig).setOrigin(0.5);
        
